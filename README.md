54c3bc117407bb3eab684f51b9dad64066914f96
Loic017 <l.loic017@gmail.com>
2025-05-15 18:38:46 +0100

    arduino implementation

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ Gesture recognition on Arduino Nano 33 BLE Sense. Goal: Train and deploy initial
 - [ ] Write model from scratch for Arduino (Move weights from PyTorch model)
 - [ ] Make gestures trigger events
 - [ ] Add more gestures
+
+## My Takeaways/Practice Areas
+- Working with Arduino
+- Edge deployment
+- C++ and writing ML in C++
--- arduino/Gesture Detection/src/main.cpp
+++ arduino/Gesture Detection/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Arduino_LSM9DS1.h>
 #include <Arduino_APDS9960.h>
+#include <state_dict.h>
 
 bool recording = false;
 int proximity = 250;
@@ -57,5 +58,45 @@ void loop() {
     }
   }
 }
+}
 
+float* dense_layer(float *input, int input_shape, int output_shape, int hidden_units, float *loaded_weights, float *loaded_biases) {
+  float weights[hidden_units][input_shape];
+  float biases[hidden_units];
+  float output[output_shape];
+
+  for (int i = 0; i < hidden_units; i++) {
+    biases[i] = loaded_biases[i];
+
+    for (int j = 0; j < input_shape; j++) {
+      weights[i][j] = loaded_weights[i * input_shape + j];
+    }
+  }
+
+  for (int i = 0; i < hidden_units; i++) {
+    output[i] = 0;
+    for (int j = 0; j < input_shape; j++) {
+      output[i] += input[j] * weights[i][j];
+    }
+    output[i] += biases[i];
+  }
+
+  return output;
+}
+
+float* relu(float *input, int input_shape, int hidden_units) {
+    for (int i = 0; i < 800; i++) {
+        if (input[i] < 0) {
+            input[i] = 0;
+        }
+    }
+    return input;
+}
+
+int dense_model(float *input) {
+  int input_shape = 800;
+  int output_shape = 3;
+  int hidden_units = 10;
+
+  return predicted_class;
 }
